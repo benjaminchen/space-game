a0b174e1d6c581a8549ecede00017008f3cddc43
benjamin <benjaminchen.c@gmail.com>
2016-08-31 16:51:09 +0800

    add rocks

--- libs/Game.js
+++ libs/Game.js
@@ -1,6 +1,8 @@
 import Back from './Back.js';
 import Ship from './Ship.js';
+import Rock from './Rock.js';
 
+// todo 雙緩衝區
 class Game {
     constructor(canvas, config) {
         this.screen = {
@@ -11,10 +13,21 @@ class Game {
         this.ctx = canvas.getContext("2d");
         this.back = new Back(config.backImage, this.screen);
         this.ship = new Ship(config.shipImage, this.screen);
+        this.rockImage = new Image();
+        this.rockImage.src = "images/rocks/rock.png";
+        this.rocks = [];
+    }
+
+    generateRock() {
+        if (new Date() % 50 != 0 || this.rocks.length > 10) return;
+        var rock;
+        rock = new Rock(this.rockImage, Math.random() * (this.screen.width - this.rockImage.width - 10), -this.rockImage.height - 5, 5);
+        this.rocks.push(rock);
     }
 
     draw() {
-        var ctx = this.ctx,
+        var me = this,
+            ctx = this.ctx,
             screen = this.screen,
             back = this.back,
             ship = this.ship;
@@ -22,10 +35,19 @@ class Game {
         ctx.clearRect(0, 0, screen.width, screen.height);
         back.draw(ctx);
         ship.draw(ctx);
+        for (var i = 0; i < this.rocks.length; i++) {
+            var rock = this.rocks[i];
+            if (rock.y > (me.screen.height + 10) || rock.exploded) {
+                this.rocks.splice(i, 1);
+            } else {
+                rock.draw(ctx);
+            }
+        }
     }
 
     start() {
         this.draw();
+        this.generateRock();
         window.requestAnimationFrame(this.start.bind(this));
     }
 }
--- libs/Rock.js
+++ libs/Rock.js
@@ -1,7 +1,21 @@
 class Rock {
-    constructor(img, screen) {}
+    constructor(img, x, y, speed) {
+        var r = Math.random();
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.width = img.width;
+        this.height = img.height;
+        this.speed = speed * r;
+        this.exploded = false;
+    }
 
-    draw(ctx) {}
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        if (! this.exploded) this.y += this.speed;
+    }
+
+    explode() {}
 }
 
 export default Rock;
