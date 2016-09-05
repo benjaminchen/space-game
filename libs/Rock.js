65c301090aa8dd87bd9bee1817d9b466a4102175
benjamin <benjaminchen.c@gmail.com>
2016-09-05 17:08:13 +0800

    add rock explotion animation

--- libs/Rock.js
+++ libs/Rock.js
@@ -7,13 +7,17 @@ class Rock {
         this.width = img.width;
         this.height = img.height;
         this.speed = speed * r;
+        this.exploding = false;
         this.exploded = false;
+        this.explodeImg = new Image();
+        this.explodeImg.src = 'images/explosion.png';
+        this.num = 0;
     }
 
     draw(ctx, bullets) {
         var me, right, bottom;
 
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        this.exploding === true ? this.explode(ctx) : ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
         if (! this.exploded) this.y += this.speed;
 
         me = this;
@@ -21,10 +25,20 @@ class Rock {
         bottom = this.y + this.height * 0.6;
         bullets.forEach(function(bullet, index) {
             if (bullet.y <= bottom && (bullet.x >= me.x && bullet.x <= right)) {
-                me.exploded = true;
+                me.exploding = true;
                 bullets.splice(index, 1);
             }
         });
+    }
+
+    explode(ctx) {
+        var remainder, quotient, x, y;
+        remainder = this.num % 4;
+        quotient = (this.num - remainder) / 4;
+        x = this.x + this.width / 2 - 32;
+        y = this.y + this.height / 2;
+        ctx.drawImage(this.explodeImg, remainder * 64, quotient * 64, 64, 64, x, y, 64, 64);
+        this.num === 15 ? this.exploded = true : this.num++;
     }
 }
 
